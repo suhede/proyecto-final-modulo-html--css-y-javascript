120afcd3445a062713b1b00017e5d0f6f7d4f493
suhede <susan101@alumnos.cei.es>
2024-01-21 11:27:26 +0100

    snapshot v0.0.1

--- javascript/index.js
+++ javascript/index.js
@@ -23,4 +23,22 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
+   // -----------EVENTO VALIDACION FORMULARIO-----------------------
+  
+// Se crea una función y dentro de esta se declaran variables que se usaran para validar el formulario,
+//en este caso se llamo por ID los inputs.
+function prepararCorreo() {
+  let nombre = document.getElementById("nombre").value;
+  let email = document.getElementById("email").value;
+  let mensaje = document.getElementById("mensaje").value;
+
+  // Crear el enlace "mailto" con los valores del formulario
+  let enlaceMailto = "mailto:?subject=Asunto del Correo&body=" +
+                     encodeURIComponent("Nombre: " + nombre + "\nCorreo electrónico: " + email + "\n\nMensaje: " + mensaje);
+
+  // Abrir la aplicación de correo predeterminada de Windows
+  window.location.href = enlaceMailto;
+}
+
+
 });
